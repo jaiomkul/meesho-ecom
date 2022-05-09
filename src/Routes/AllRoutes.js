d316de4018eecac8a5e0e2f34209b35842709b0e
jaiomkul <jaygupta2601@gmail.com>
2022-05-09 20:35:09 +0530

    Update AllRoutes.js

--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -1,9 +1,9 @@
 import { Route, Switch } from "react-router-dom";
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from "history";
 import CartRoutes from "./CartRoutes";
 import NavBar from "../Components/Navbar";
 import Home from "../Pages/home";
-import Category from "../Pages/catagorypage"
+import Category from "../Pages/catagorypage";
 import { SignUp } from "../Pages/SignUp";
 import { Productadd } from "../Pages/SingleDetailProduct";
 import { Otp } from "../Pages/Otp";
@@ -16,90 +16,86 @@ import Cart from "../Pages/Cart/Cart";
 import About from "../Components/NavBar/About";
 import Footer from "../Components/NavBar/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Upperbar from "../Pages/Seller/seller_home/Navbar_seller"
-import Lowerbar from "../Pages/Seller/seller_home/Footer_seller"
-import Sellerlogin from "../Pages/Seller/sellerloginsignup/sellerlgin"
-import SellerData from "../Pages/Seller/seller_data/sellerData"
+import Upperbar from "../Pages/Seller/seller_home/Navbar_seller";
+import Lowerbar from "../Pages/Seller/seller_home/Footer_seller";
+import Sellerlogin from "../Pages/Seller/sellerloginsignup/sellerlgin";
+import SellerData from "../Pages/Seller/seller_data/sellerData";
 
- const newHistory = createBrowserHistory();
-const AllRoutes = ()=>{
-    return (
-        <>
-                <Switch>
-                    <Route exact path="/">
-                        <NavBar />
-                        <Home/>
-                        <Footer />
-                      <About />
-                    </Route>
-                    <Route exact={true} path="/category">
-                        <NavBar/>
-                        <Category/>
-                        <Footer />
-                        <About />
-                    </Route>
-                    <Route exact={true} path="/signup">
-                        <SignUp />
-                        <Footer />
-                      <About />
-                    </Route>
-                    <Route exact={true} path="/otp">
+const newHistory = createBrowserHistory();
+const AllRoutes = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <NavBar />
+          <Home />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact={true} path="/category">
+          <NavBar />
+          <Category />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact={true} path="/signup">
+          <SignUp />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact={true} path="/otp">
+          <Otp />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact={true} path="/sellerLogin">
+          <Sellerlogin />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact={true} path="/sellerData">
+          <NavBar />
+          <SellerData />
+          <Footer />
+          <About />
+        </Route>
 
-                        <Otp />
-                        <Footer />
-                        <About />
-                    </Route>
-                    <Route exact={true} path="/sellerLogin">
+        <Route exact path="/cart/checkout/form">
+          <CheckoutPage />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact path="/cart/payment/detail">
+          <PaymentPage />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact path="/cart/summary/detail">
+          <SummaryPage />
+          <Footer />
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+          <Footer />
+          {/* <About /> */}
+        </Route>
+        <Route exact={true} path="/:q/:id">
+          <NavBar />
+          <Productadd />
+        </Route>
+        <Route exact={true} path="/seller">
+          {/* <AddProductPage /> */}
+          <Upperbar />
+          <Login_input />
+          <Lowerbar />
+        </Route>
 
-                      <Sellerlogin/>
-                        <Footer />
-                        <About />
-                    </Route>
-                    <Route exact={true} path="/sellerData">
-                    <NavBar/>
-                       <SellerData/>
-                       <Footer />
-                         <About />
-                           </Route> 
-
-                    <Route exact path="/cart/checkout/form"> 
-                     <CheckoutPage />
-                     <Footer />
-                     <About />
-                    </Route> 
-                    <Route exact path="/cart/payment/detail">
-
-                    <PaymentPage />
-                    <Footer />
-                   <About />
-                  </Route>
-                  <Route exact path="/cart/summary/detail">
-                    <SummaryPage />
-                    <Footer />
-                  <About />
-                     </Route>
-                     <Route exact path="/cart">
-                      <Cart />
-                      <Footer />
-                     <About />
-                     </Route>
-                    <Route exact={true} path="/:q/:id">
-                        <NavBar/>
-                        <Productadd />
-                    </Route>
-                    <Route exact={true} path="/seller">
-                        
-                        {/* <AddProductPage /> */}
-                        <Upperbar/>
-                        <Login_input/>
-                       <Lowerbar/>
-                    </Route>
-                    
-                    <Route path="/cart">
-                        <CartRoutes />
-                    </Route>
-                </Switch>
-        </>
-    )
-}
-export default AllRoutes
+        <Route path="/cart">
+          <CartRoutes />
+        </Route>
+      </Switch>
+    </>
+  );
+};
+export default AllRoutes;
